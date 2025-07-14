36c5b4b65b160510f42b7b87aea609973d39b103
Utkarsh Shukla <shuklautkarsh406@gmail.com>
2025-07-14 23:56:13 +0530

    Create leetcode367.java

---
+++ leetcode367.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low=1;
+        int high=num;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long sq=(long)mid*mid;
+            if(sq==num){
+                return true;
+            }
+            else if(sq<num) low=mid+1;
+            else
+            high=mid-1;
+        }
+        return false;
+    }
+}
